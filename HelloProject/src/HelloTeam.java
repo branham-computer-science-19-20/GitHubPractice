882c96dfaee5933902e57eeb3981703e467f9609
ZacheryByrd <54453310+ZacheryByrd@users.noreply.github.com>
2020-04-28 14:00:02 -0700

    Update HelloTeam.java

--- HelloProject/src/HelloTeam.java
+++ HelloProject/src/HelloTeam.java
@@ -4,10 +4,9 @@ public class HelloTeam {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Greetings, Mr. Turner!");
-		System.out.println("Hello, Branham!");
+		System.out.println("Hello, Zach!");
 		System.out.println("Hello students!");
 		System.out.println("Salutations to Mr. Turner's new room J205!");
-		System.out.println("Aayushi Neema");
 	}
 
 }
